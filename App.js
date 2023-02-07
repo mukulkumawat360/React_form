c5456a432b3b7559b7cd4a312bbd572e918156ff
Mukul Kumawat <66302237+mukulkumawat360@users.noreply.github.com>
2023-02-08 00:00:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Page  from './Question/Page.js';
+import StartPage from './Question/StartPage';
+
+
+
+
+function App() {
+ 
+  return (
+    <>
+    
+    <div className="App"  style={{backgroundColor:"#AFDBF5",width:"1260px",height:"600px",paddingTop:"1px"}}>
+    <StartPage/>
+    </div>
+
+    
+    </>
+    
+  );
+}
+
+export default App;
